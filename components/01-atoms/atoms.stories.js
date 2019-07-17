7c016260eff47200fa8f0b161c33bff339f351fd
Evan Willhite <evan@fourkitchens.com>
2019-07-17 08:39:36 -0400

    clean up storybook buttons

--- components/01-atoms/atoms.stories.js
+++ components/01-atoms/atoms.stories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
-import Button from './button/Button.component';
+import Button from './button-react/Button.component';
 
 import button from './button/button.twig'
 const buttonTwig = (
@@ -13,9 +13,9 @@ const buttonTwig = (
  * Add storybook definition for Button.
  */
 storiesOf('Atoms/Button', module)
-  .add('Default', () => (
+  .add('React button', () => (
     <Button onClick={action('button-clicked')}>Click Here</Button>
   ))
-  .add('button', () => 
+  .add('Twig button', () => 
     <div dangerouslySetInnerHTML={{__html: buttonTwig}}></div>
   )
