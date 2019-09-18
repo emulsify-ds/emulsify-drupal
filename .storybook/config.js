d2e40f232f9c2eb607c79aa3d10599a01332605c
Evan Willhite <evan@fourkitchens.com>
2019-09-18 13:29:31 -0500

    Twig extensions working in Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -7,9 +7,14 @@ import '../dist/styles.css';
 // GLOBAL JS
 import '../dist/js/main.bundle.js';
 
+const Twig = require('twig')
 const twigDrupal = require('twig-drupal-filters')
 const twigBEM = require('bem-twig-extension');
 const twigAddAttributes = require('add-attributes-twig-extension');
+
+twigDrupal(Twig);
+twigBEM(Twig);
+twigAddAttributes(Twig);
 
  // automatically import all files ending in *.stories.js
 const req = require.context("../components", true, /.stories.js$/)
@@ -17,8 +22,19 @@ function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
 
+const context = require.context('../components', true, /\.twig$/)
+context.keys().forEach(key => {
+  var template = context(key);
+  Twig.twig({
+    id: key,
+    data: template.tokens,
+    allowInlineIncludes: true,
+    rethrow: true
+  });
+});
+
 // twigDrupal(Twig);
-twigBEM(Twig);
+
 // twigAddAttributes(Twig);
 
 
