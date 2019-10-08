4bae00997dcdf9ffb6fce5774ae738e2ff5d43b3
Evan Willhite <evan@fourkitchens.com>
2019-10-08 15:32:10 -0500

    text stuff, Twig cache clear

--- components/01-atoms/atoms.stories.js
+++ components/01-atoms/atoms.stories.js
@@ -31,6 +31,31 @@ storiesOf('Atoms/Headings', module)
     <div dangerouslySetInnerHTML={{__html: headings({})}}></div>
   ))
 
+import paragraph from './02-text/text/03-inline-elements.twig'
+
+import blockquote from './02-text/text/02-blockquote.twig'
+const blockquoteComponent = (
+  blockquote({
+    blockquote_content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+  })
+)
+
+import pre from './02-text/text/05-pre.twig'
+
+/**
+ * Add storybook definition for Links.
+ */
+storiesOf('Atoms/Text', module)
+  .add('Various', () => (
+    <div dangerouslySetInnerHTML={{__html: paragraph({})}}></div>
+  ))
+  .add('Blockquote', () => (
+    <div dangerouslySetInnerHTML={{__html: blockquoteComponent}}></div>
+  ))
+  .add('Preformatted', () => (
+    <div dangerouslySetInnerHTML={{__html: pre({})}}></div>
+  ))
+
 
 import Button from './06-buttons/react/Button.component';
 
