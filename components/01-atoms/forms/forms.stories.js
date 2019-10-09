8948fa9716175586d614249b9633454e19ba352a
Evan Willhite <evan@fourkitchens.com>
2019-10-09 14:46:47 -0500

    select and textfields

--- components/01-atoms/forms/forms.stories.js
+++ components/01-atoms/forms/forms.stories.js
@@ -43,6 +43,31 @@ const radioTwig = (
   })
 )
 
+import select from './select/select.twig'
+const selectTwig = (
+  select({
+    select: {
+      0: {
+        title: "Choose an Option"
+      },
+      1: {
+        title: "Option 1"
+      },
+      2: {
+        title: "Option 2"
+      },
+      3: {
+        title: "Option 3"
+      },
+      4: {
+        title: "Option 4"
+      }
+    }
+  })
+)
+
+import textfields from './textfields/textfields.twig'
+
 /**
  * Add storybook definition for Buttons.
  */
@@ -52,4 +77,10 @@ storiesOf('Atoms/Forms', module)
   ))
   .add('Radio Buttons', () => (
     <div dangerouslySetInnerHTML={{__html: radioTwig}}></div>
+  ))
+  .add('Select Dropdown', () => (
+    <div dangerouslySetInnerHTML={{__html: selectTwig}}></div>
+  ))
+  .add('Textfields', () => (
+    <div dangerouslySetInnerHTML={{__html: textfields({})}}></div>
   ))
