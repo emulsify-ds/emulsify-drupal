c0e8be4a802fd7e04456422d3979f6c85588547c
Evan Willhite <evan@fourkitchens.com>
2019-10-15 16:33:02 -0500

    linking pages

--- components/05-pages/landing-pages/landing-pages.stories.js
+++ components/05-pages/landing-pages/landing-pages.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { hrefTo } from '@storybook/addon-links';
 
 import { page_layout_modifier, breadcrumb, social_menu__items, footer_menu__items } from '../../../.storybook/globalData';
 
@@ -8,7 +9,9 @@ import home from './home.twig';
 /**
  * Add storybook definition for Landing Pages.
  */
-storiesOf('Pages/Landing Pages', module)
-  .add('Home', () =>
-    <div dangerouslySetInnerHTML={{__html: home({ page_layout_modifier, breadcrumb, social_menu__items, footer_menu__items })}}></div>
-  )
+hrefTo('Pages/Content Types', 'Article').then(url => {
+  storiesOf('Pages/Landing Pages', module)
+    .add('Home', () =>
+      <div dangerouslySetInnerHTML={{__html: home({ page_layout_modifier, breadcrumb, social_menu__items, footer_menu__items, card_link_url: url })}}></div>
+    )
+});
