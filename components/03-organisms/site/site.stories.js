782786432b25d4b75654d7289251073893c29879
Evan Willhite <evan@fourkitchens.com>
2019-11-14 20:12:38 -0600

    path fixes, emulsify.php WIP

--- components/03-organisms/site/site.stories.js
+++ components/03-organisms/site/site.stories.js
@@ -8,6 +8,7 @@ import footerSocial from '../../02-molecules/menus/social/social-menu.yml';
 import footerMenu from '../../02-molecules/menus/inline/inline-menu.yml';
 import breadcrumbData from '../../02-molecules/menus/breadcrumbs/breadcrumbs.yml';
 import mainMenubData from '../../02-molecules/menus/main-menu/main-menu.yml';
+import siteHeaderData from './site-header/site-header.yml';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -22,7 +23,7 @@ export const header = () => {
   useEffect(() => Drupal.attachBehaviors(), []);
   return (
     <div
-      dangerouslySetInnerHTML={{ __html: siteHeader({ ...breadcrumbData, ...mainMenubData }) }}
+      dangerouslySetInnerHTML={{ __html: siteHeader({ ...breadcrumbData, ...mainMenubData, ...siteHeaderData }) }}
     />
   );
 };
