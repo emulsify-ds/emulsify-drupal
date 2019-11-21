bc8b0f1553ccd3a9aa82438df1d2b3c20d6c7efe
Evan Willhite <evan@fourkitchens.com>
2019-11-21 09:11:29 -0600

    logo change

--- .storybook/emulsifyTheme.js
+++ .storybook/emulsifyTheme.js
@@ -7,5 +7,5 @@ export default create({
   // Branding
   brandTitle: 'Emulsify',
   brandUrl: 'https://emulsify.info',
-  brandImage: 'images/logo.png',
+  brandImage: 'https://raw.githubusercontent.com/emulsify-ds/emulsify-design-system/master/images/logo.png',
 });
