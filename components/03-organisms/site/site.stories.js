ed300ad503b47fd491820412d1360ae22e9a49bc
Brian Lewis <brian@modulesunraveled.com>
2020-02-05 12:09:57 -0600

    Deleted site-header.yml since it only defined a path that was already hard-coded in the twig.

--- components/03-organisms/site/site.stories.js
+++ components/03-organisms/site/site.stories.js
@@ -7,8 +7,7 @@ import siteHeader from './site-header/site-header.twig';
 import footerSocial from '../../02-molecules/menus/social/social-menu.yml';
 import footerMenu from '../../02-molecules/menus/inline/inline-menu.yml';
 import breadcrumbData from '../../02-molecules/menus/breadcrumbs/breadcrumbs.yml';
-import mainMenubData from '../../02-molecules/menus/main-menu/main-menu.yml';
-import siteHeaderData from './site-header/site-header.yml';
+import mainMenuData from '../../02-molecules/menus/main-menu/main-menu.yml';
 
 import '../../02-molecules/menus/main-menu/main-menu';
 
@@ -24,7 +23,7 @@ export const header = () => {
   return (
     <div
       dangerouslySetInnerHTML={{
-        __html: siteHeader({ ...breadcrumbData, ...mainMenubData, ...siteHeaderData }),
+        __html: siteHeader({ ...breadcrumbData, ...mainMenuData }),
       }}
     />
   );
