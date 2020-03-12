d035a520ddda4a7cb2e1e215bef6d8325bd04c90
Aaron Couch <acinternets@gmail.com>
2020-03-12 11:17:13 -0400

    Update text.stories.js

--- components/01-atoms/text/text.stories.js
+++ components/01-atoms/text/text.stories.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import headings from './headings/headings.twig';
+import heading from './headings/_heading.twig';
 import blockquote from './text/02-blockquote.twig';
 import pre from './text/05-pre.twig';
 import paragraph from './text/03-inline-elements.twig';
 
 import blockquoteData from './text/blockquote.yml';
+import headingData from './headings/headings.yml';
 
 /**
  * Storybook Definition.
  */
 export default { title: 'Atoms/Text' };
 
+const headings = headingData.map(d => heading(d));
+
 export const headingsExamples = () => (
-  <div dangerouslySetInnerHTML={{ __html: headings({}) }} />
+  <div dangerouslySetInnerHTML={{ __html: headings }} />
 );
 export const blockquoteExample = () => (
   <div dangerouslySetInnerHTML={{ __html: blockquote(blockquoteData) }} />
