abb11f98627b7c847616e35a49281f7f2842761a
Evan Willhite <evan@fourkitchens.com>
2020-11-10 14:34:52 -0600

    storybook upgrade, moving storybook theme to manager per docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,20 +3,11 @@ import { useEffect } from '@storybook/client-api';
 import Twig from 'twig';
 import { setupTwig } from './setupTwig';
 
-// Theming
-import emulsifyTheme from './emulsifyTheme';
-
 // GLOBAL CSS
 import '../components/style.scss';
 
 // If in a Drupal project, it's recommended to import a symlinked version of drupal.js.
 import './_drupal.js';
-
-export const parameters = {
-  options: {
-    theme: emulsifyTheme,
-  },
-};
 
 // addDecorator deprecated, but not sure how to use this otherwise.
 addDecorator((storyFn) => {
