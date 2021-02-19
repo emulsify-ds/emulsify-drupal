a42150235186f9fc924fc671e2f1e8e069d137bd
Evan Willhite <evan@fourkitchens.com>
2021-02-18 21:45:46 -0600

    feat(docs): add docs example

--- components/00-base/01-colors/colors.stories.js
+++ components/00-base/01-colors/colors.stories.js
@@ -2,9 +2,18 @@ import colors from './colors.twig';
 
 import colorsData from './colors.yml';
 
+import colorsDocs from './colors.mdx';
+
 /**
  * Storybook Definition.
  */
-export default { title: 'Base/Colors' };
+export default {
+  title: 'Base/Colors',
+  parameters: {
+    docs: {
+      page: colorsDocs,
+    },
+  },
+};
 
 export const Palettes = () => colors(colorsData);
