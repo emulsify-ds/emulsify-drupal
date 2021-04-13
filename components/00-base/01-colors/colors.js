ef07730d7b3092e55065c41a6d2ead251039fc26
Callin Mullaney <callin@mullaney.io>
2021-04-12 23:45:50 -0500

    feat: Adding color definitions and usuage to the palette page

--- components/00-base/01-colors/colors.js
+++ components/00-base/01-colors/colors.js
@@ -1,21 +1,16 @@
-function unquoted(el) {
-  return el.replace(/(^['"])|(['"]$)/g, '');
-}
+Drupal.behaviors.displayColorDefinitions = {
+  attach() {
+    function unquoted(el) {
+      return el.replace(/(^['"])|(['"]$)/g, '');
+    }
+    const elements = document.getElementsByClassName('cl-colors__definition');
 
-const elements = document.getElementsByClassName('cl-colors__definition');
-
-elements.forEach((element) => {
-  const styles = window.getComputedStyle(element, '::before');
-  const content = styles.getPropertyValue('content');
-  const child = document.createElement('span');
-  child.innerHTML = unquoted(content);
-
-  // eslint-disable-next-line no-param-reassign
-  element.innerHTML += child;
-
-  // eslint-disable-next-line no-console
-  console.log(element);
-
-  // eslint-disable-next-line no-console
-  console.log(child);
-});
+    // eslint-disable-next-line func-names
+    Array.prototype.forEach.call(elements, function (element) {
+      const styles = window.getComputedStyle(element, '::before');
+      const content = styles.getPropertyValue('content');
+      // eslint-disable-next-line no-param-reassign
+      element.innerHTML = unquoted(content);
+    });
+  },
+};
