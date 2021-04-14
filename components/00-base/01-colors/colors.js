d1a2082f4eb256397726a1965bf0aac9f6082bd5
Callin Mullaney <callin@mullaney.io>
2021-04-14 08:42:06 -0500

    refactor: Code cleanup and elaborating on existing comments

--- components/00-base/01-colors/colors.js
+++ components/00-base/01-colors/colors.js
@@ -7,10 +7,18 @@ Drupal.behaviors.displayColorDefinitions = {
 
     // eslint-disable-next-line func-names
     Array.prototype.forEach.call(elements, function (element) {
-      const styles = window.getComputedStyle(element, '::before');
-      const content = styles.getPropertyValue('content');
-      // eslint-disable-next-line no-param-reassign
-      element.innerHTML = unquoted(content);
+      const stylesBefore = window.getComputedStyle(element, '::before');
+      const stylesAfter = window.getComputedStyle(element, '::after');
+      const contentBefore = stylesBefore.getPropertyValue('content');
+      const contentAfter = stylesAfter.getPropertyValue('content');
+      const spanBefore = document.createElement('span');
+      const spanAfter = document.createElement('span');
+      spanBefore.classList.add('cl-colors__definition_item');
+      spanAfter.classList.add('cl-colors__definition_item');
+      spanBefore.innerHTML = unquoted(contentBefore);
+      spanAfter.innerHTML = unquoted(contentAfter);
+      element.appendChild(spanBefore);
+      element.appendChild(spanAfter);
     });
   },
 };
