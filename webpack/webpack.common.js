de6270c9c25abf0e76b851380cbadadcf616e046
Andy Sipple <andysipple@andysipbpkanopi.lan>
2021-11-10 08:20:14 -1000

    Point directly to style.scss file instead of using css.js as entry

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -17,7 +17,7 @@ function getEntries(pattern) {
   });
 
   entries.svgSprite = path.resolve(webpackDir, 'svgSprite.js');
-  entries.css = path.resolve(webpackDir, 'css.js');
+  entries.css = path.resolve(webpackDir, '../components/style.scss');
 
   return entries;
 }
