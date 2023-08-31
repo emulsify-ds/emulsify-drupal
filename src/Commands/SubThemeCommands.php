3d7e5274430303021664d57e5a429a0b21314b56
feynman <callin@mullaney.io>
2023-08-31 13:55:09 -0500

    feat: update class comments for drush command

--- src/Commands/SubThemeCommands.php
+++ src/Commands/SubThemeCommands.php
@@ -18,7 +18,15 @@ use Robo\Task\Filesystem\Tasks as FilesystemTaskLoader;
 use Symfony\Component\Finder\Finder;
 
 /**
- * Defines Drush commands for generating emulsify subtheme.
+ * A Drush commandfile.
+ *
+ * In addition to this file, you need a drush.services.yml
+ * in root of your module, and a composer.json file that provides the name
+ * of the services file to use.
+ *
+ * See these files for an example of injecting Drupal services:
+ *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
+ *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
  */
 class SubThemeCommands extends DrushCommands implements BuilderAwareInterface {
 
