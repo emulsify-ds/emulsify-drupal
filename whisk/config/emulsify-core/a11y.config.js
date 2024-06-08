4247d9cc9a6c021adbbbd36a5ac0c452e702f4e5
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-06-07 21:00:44 -0500

    feat: update whisk to use official emulsify-core release
    BREAKING CHANGE

---
+++ whisk/config/emulsify-core/a11y.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // A11y linting is done on a component-by-component
+  // basis, which results in the linter reporting some errors that
+  // should be ignored. These codes and descriptions allow for those
+  // errors to be targeted specifically.
+  ignore: {
+    codes: [],
+    descriptions: [],
+  },
+  // List of storybook component IDs defined and used in this project.
+  components: [],
+};
