f8250937d7693dbd37796ae63c9fa55e80c43d01
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2025-05-16 15:18:19 -0500

    feat: provide an empty overrideParams for emulsify-core if the project needs to use it

--- whisk/config/emulsify-core/storybook/preview.js
+++ whisk/config/emulsify-core/storybook/preview.js
@@ -1,25 +1,5 @@
-// export global parameters as config overrides.
-// This is useful for reorganizing your stories.
+// export project specific parameters as config overrides.
 // See https://storybook.js.org/docs/writing-stories/parameters#story-parameters.
-const overrideParams = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  layout: 'fullscreen',
-  options: {
-    storySort: {
-      method: 'alphabetical',
-      order: [
-        'Docs',
-        ['Intro', '*'],
-        'Tokens',
-        ['Readme', '*'],
-        'Components',
-        ['Readme', '*'],
-        'Layout',
-        ['Readme', '*'],
-      ],
-      locales: 'en-US',
-    },
-  },
-};
+const overrideParams = {};
 
 export default overrideParams;
