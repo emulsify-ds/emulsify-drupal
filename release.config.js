23c20bb00400d38515a3b56ca9a52cc014d67602
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2025-05-26 17:36:30 -0500

    feat: add paragraph and layout preprocesses

--- release.config.js
+++ release.config.js
@@ -1,11 +1,31 @@
+// release.config.js
 module.exports = {
   tagFormat: '${version}',
   branches: ['main'],
   repositoryUrl: 'git@github.com:emulsify-ds/emulsify-drupal.git',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        }
+      }
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'angular',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING']
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope']
+        }
+      }
+    ],
     ['@semantic-release/npm', { npmPublish: false }],
-    '@semantic-release/github',
-  ],
-};
+    '@semantic-release/github'
+  ]
+}
